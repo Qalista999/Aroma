fa7366f1639e084e2272462869b0914774a408f5
Qalista999 <116348527+Qalista999@users.noreply.github.com>
2022-11-22 12:10:24 +0300

    php + pages

---
+++ router.php
@@ -0,0 +1,32 @@
+<?php
+// $url = $_SERVER['REQUEST_URI'];
+// // $url = explode('?', $url);
+// // $url = $url[0];
+
+// echo $url;
+$url = explode("/", $_SERVER['REQUEST_URI']);
+// for($i = 0; $i < count($url); $i++) {
+// echo $url[$i]."<hr>";
+// }
+// if ($url[1] == "login") {
+// require_once("login.php");
+// }
+if ($url[1] == "blog") {
+$content = file_get_contents("pages/blog.html");
+} else if ($url[1] == "contact"){
+$content = file_get_contents("pages/contact.html");
+} else if ($url[1] == "login"){
+$content = file_get_contents("pages/login.php");
+} else if ($url[1] == "register") {
+$content = file_get_contents("pages/register.html");
+} else if ($url[1] == "articles") {
+$content = file_get_contents("pages/single-blog.html");
+} else if ($url[1] == "users") {
+require_once("pages/users/index.html");
+}
+else {
+$content = file_get_contents("pages/index.php");
+}
+
+if (!empty($content))
+require_once("template.php");
