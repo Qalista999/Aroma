bfe735f16e43ebe6efde15b453f2db82eab5f742
Qalista999 <116348527+Qalista999@users.noreply.github.com>
2022-11-24 23:33:19 +0300

    где-то есть ошибка

--- router.php
+++ router.php
@@ -1,10 +1,13 @@
 <?php
+session_start();
 // $url = $_SERVER['REQUEST_URI'];
 // // $url = explode('?', $url);
 // // $url = $url[0];
 
 // echo $url;
 $url = explode("/", $_SERVER['REQUEST_URI']);
+require_once("php/bd.php");
+require_once("php/classes/User.php");
 // for($i = 0; $i < count($url); $i++) {
 // echo $url[$i]."<hr>";
 // }
@@ -13,9 +16,9 @@ $url = explode("/", $_SERVER['REQUEST_URI']);
 // }
 if ($url[1] == "blog") {
 $content = file_get_contents("pages/blog.html");
-} else if ($url[1] == "contact"){
+} else if ($url[1] == "contact") {
 $content = file_get_contents("pages/contact.html");
-} else if ($url[1] == "login"){
+} else if ($url[1] == "auth") {
 $content = file_get_contents("pages/login.php");
 } else if ($url[1] == "register") {
 $content = file_get_contents("pages/register.html");
@@ -23,8 +26,11 @@ $content = file_get_contents("pages/register.html");
 $content = file_get_contents("pages/single-blog.html");
 } else if ($url[1] == "users") {
 require_once("pages/users/index.html");
-}
-else {
+} else if ($url[1] == "addUser") {
+echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "authUser") {
+echo User::authUser($_POST[$email], $_POST["pass"]);
+} else {
 $content = file_get_contents("pages/index.php");
 }
 
